832b5cb223c7710139611a65d3487d4c2b3ef92f
Sandie Nunez <snunez@vt.edu>
2020-04-04 21:50:37 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,10 +1,10 @@
 def prime?(integer)
-  if integer < 0 || integer = 0 || integer = 1 
+  if integer < 0 || integer == 0 || integer == 1 
     return false
   else 
     counter = 2 
     while counter < integer do 
-      if integer % counter = 0 
+      if integer % counter == 0 
         return true 
       end
     end
