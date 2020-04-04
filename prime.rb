f311c54d07041193a78b03e297cfde1646cfb877
Sandie Nunez <snunez@vt.edu>
2020-04-04 22:04:41 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,16 @@
-# Add  code here!
+def prime?(integer)
+  if integer < 0 || integer == 0 || integer == 1 
+    return false
+  elsif integer == 2
+    return true 
+  else 
+    counter = 2 
+    while counter < integer do 
+      if integer % counter == 0 
+        return false 
+      end
+      counter = counter + 1 
+    end
+  end
+  return true 
+end 
